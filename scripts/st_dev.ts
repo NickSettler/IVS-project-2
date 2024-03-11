c77944c60f62c8f06da5d2e052b8fc371260beee
NickSettler <moiseevnikita14@gmail.com>
2024-03-11 08:25:37 +0100

    feat(script): add standard deviation calculation script

---
+++ scripts/st_dev.ts
@@ -0,0 +1,23 @@
+import { Lexer, Scanner, Executor } from '../src/lib/calc';
+import { readFileSync } from 'fs';
+
+const STDIN_FILENO = 0;
+
+const input = readFileSync(STDIN_FILENO, 'utf-8');
+
+const processedInput = input
+  .replace(/[\n\s\t\r]/g, ',')
+  .replace(/,{2,}/g, ',')
+  .replace(/,$/, '');
+
+const calc = (expression: string): number => {
+  const lexer = new Lexer(expression);
+  const tree = new Scanner(lexer.getNextToken.bind(lexer)).processQuery();
+  const executor = new Executor(tree);
+
+  return executor.execute();
+};
+
+const expression = `stddev([${processedInput}])`;
+
+console.log(calc(expression));
