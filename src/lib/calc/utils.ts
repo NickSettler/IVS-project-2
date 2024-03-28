e5e82f225beabd53848533fe485830ba93586806
NickSettler <moiseevnikita14@gmail.com>
2024-03-28 14:57:08 +0100

    refactor(lib): remove unused function

--- src/lib/calc/utils.ts
+++ src/lib/calc/utils.ts
@@ -12,16 +12,3 @@ export const isExpressionValid = (expression: string): boolean => {
     return false;
   }
 };
-
-export const isExpressionComputable = (expression: string): boolean => {
-  try {
-    const lexer = new Lexer(expression);
-    const tree = new Scanner(lexer.getNextToken.bind(lexer)).processQuery();
-    const executor = new Executor(tree);
-    executor.execute();
-
-    return true;
-  } catch (e: any) {
-    return false;
-  }
-};
