54d95de3a499ee6e60fbcb1b123e51ccc6251987
Nick Settler <moiseevnikita14@gmail.com>
2024-04-24 06:19:29 +0200

    refactor: change data generation script for stddev

--- scripts/gen.js
+++ scripts/gen.js
@@ -4,12 +4,19 @@ const randomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const generate = (count) => {
+const generate = (count, min, max) => {
   const result = [];
   for (let i = 0; i < count; i++) {
-    result.push(randomInt(500000, 2500000));
+    result.push(randomInt(min, max));
   }
   return result;
 };
 
-fs.writeFileSync('data.txt', generate(1000).join(','));
+const min = 10;
+const max = 3000;
+const count = 1000;
+
+fs.writeFileSync(
+  `data-${count}-<${min}, ${max}>.txt`,
+  generate(count, min, max).join(',')
+);
